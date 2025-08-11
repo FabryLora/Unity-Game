0599e3aa4f9bc72aae99463cb5bf7e3ca889b0b9
Fabrizio Lora <fabrizio.caleb.lora@gmail.com>
2025-08-11 08:54:19 -0300

    taking damage

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,9 +1,27 @@
+using System;
 using UnityEngine;
 
 public class Enemy : Entity {
 
-    public void Start() {
+    private bool playerDetected;
 
+    protected override void Update() {
+
+        HandleCollisions();
+        HandleAnimation();
+        HandleFlip();
+        HandleMovement(facingDir);
+        HandleAttack(playerDetected);
+
+    }
+
+    protected override void HandleCollisions() {
+        base.HandleCollisions();
+        playerDetected = Physics2D.OverlapCircle(attackPoint.position, attackRadius, whatIsTarget);
+    }
+
+    protected override void HandleAnimation() {
+        anim.SetFloat("xVelocity", rb.linearVelocity.x);
     }
 
 }
--- Assets/Scripts/Entity.cs
+++ Assets/Scripts/Entity.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.Android;
@@ -8,8 +9,20 @@ public class Entity : MonoBehaviour {
 
     protected Animator anim;
     protected Rigidbody2D rb;
+    protected Collider2D col;
+    protected SpriteRenderer sr;
+
+
+
     [SerializeField] private LayerMask groundLayer;
     public Collider2D[] enemyColliders;
+
+    [Header("Health")]
+    [SerializeField] int maxHealth = 1;
+    [SerializeField] int currentHealth;
+    [SerializeField] private Material damageFeedbackMaterial;
+    [SerializeField] private float damageFeedbackDuration = .2f;
+    [SerializeField] private Coroutine damageFeedbackCoroutine;
 
     [Header("Attack details")]
     [SerializeField] protected float attackRadius = 1f;
@@ -17,30 +30,34 @@ public class Entity : MonoBehaviour {
     [SerializeField] protected LayerMask whatIsTarget;
 
 
+    [Header("Movement details")]
     private float xInput;
     [SerializeField] protected float speed = 8f;
     [SerializeField] private float jumpForce = 12f;
     [SerializeField] protected float groundCheck = 1.4f;
-
-
+    protected int facingDir = 1;
     protected bool isGrounded;
     protected bool facingRight = true;
     protected bool canMove = true;
-    protected bool canJump = true;
+    private bool canJump = true;
 
 
     public void Awake() {
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 3.5f;
         rb.linearDamping = 0.5f; // Adjust gravity scale for better jump feel
+        col = GetComponent<Collider2D>();
         anim = GetComponentInChildren<Animator>();
+        sr = GetComponentInChildren<SpriteRenderer>();
+        currentHealth = maxHealth;
     }
 
 
 
     protected virtual void Update() {
+
         HandleCollisions();
-        HandleMovement();
+        HandleMovement(xInput);
         HandleInput();
         HandleAnimation();
         HandleFlip();
@@ -50,14 +67,41 @@ public class Entity : MonoBehaviour {
     public void DamageTargets() {
         Collider2D[] enemyColliders = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, whatIsTarget);
 
+
         foreach (Collider2D enemy in enemyColliders) {
             enemy.GetComponent<Entity>()?.TakeDamage();
 
         }
     }
 
+    private IEnumerator DamageFeedbackCoroutine() {
+        Material originalMaterial = sr.material;
+
+        sr.material = damageFeedbackMaterial;
+
+        yield return new WaitForSeconds(damageFeedbackDuration);
+
+        sr.material = originalMaterial;
+    }
+
     private void TakeDamage() {
-        throw new NotImplementedException();
+        currentHealth--;
+
+        if (damageFeedbackCoroutine != null) {
+            StopCoroutine(damageFeedbackCoroutine);
+        }
+
+        StartCoroutine(DamageFeedbackCoroutine());
+
+        if (currentHealth <= 0) Die();
+
+    }
+
+    protected virtual void Die() {
+        anim.enabled = false;
+        col.enabled = false;
+        rb.gravityScale = 12;
+        rb.linearVelocity = new Vector2(rb.linearVelocity.x, 15);
     }
 
     public void EnableMovementAndJump(bool enable) {
@@ -65,7 +109,7 @@ public class Entity : MonoBehaviour {
         canJump = enable;
     }
 
-    protected void HandleAnimation() {
+    protected virtual void HandleAnimation() {
         anim.SetFloat("xVelocity", rb.linearVelocity.x);
         anim.SetFloat("yVelocity", rb.linearVelocity.y);
         anim.SetBool("isGrounded", isGrounded);
@@ -75,18 +119,19 @@ public class Entity : MonoBehaviour {
         xInput = Input.GetAxisRaw("Horizontal");
         if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W)) TryToJump();
 
-        if (Input.GetKeyDown(KeyCode.Mouse0)) TryToAttack();
+        if (Input.GetKeyDown(KeyCode.Mouse0)) HandleAttack(isGrounded);
     }
 
-    protected virtual void TryToAttack() {
-        if (isGrounded) {
+    protected virtual void HandleAttack(bool condition) {
+        if (condition) {
             anim.SetTrigger("attack");
             rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
         }
     }
 
-    protected virtual void HandleMovement() {
-        if (canMove) rb.linearVelocity = new Vector2(xInput * speed, rb.linearVelocity.y);
+    protected virtual void HandleMovement(float xDirection) {
+        if (canMove) rb.linearVelocity = new Vector2(xDirection * speed, rb.linearVelocity.y);
+        else rb.linearVelocity = new Vector2(0, rb.linearVelocity.y);
 
     }
 
@@ -100,7 +145,7 @@ public class Entity : MonoBehaviour {
         }
     }
 
-    private void HandleFlip() {
+    protected void HandleFlip() {
         if (rb.linearVelocity.x > 0 && !facingRight) {
             Flip();
         }
@@ -109,9 +154,10 @@ public class Entity : MonoBehaviour {
         }
     }
 
-    private void Flip() {
+    protected void Flip() {
         transform.Rotate(0f, 180f, 0f);
         facingRight = !facingRight;
+        facingDir *= -1;
     }
 
     private void OnDrawGizmos() {
